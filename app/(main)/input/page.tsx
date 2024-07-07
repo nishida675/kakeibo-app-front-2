6655da056f77665854deffd96e26492bde4a4307
nishida675 <hoge93141@gmail.com>
2024-07-08 01:36:53 +0900

    7/8

--- app/(main)/input/page.tsx
+++ app/(main)/input/page.tsx
@@ -1,11 +1,12 @@
+import NewBalanceForm from '@/components/NewBalanceForm/NewBalanceForm'
 import React from 'react'
 
 const inputPage = () => {
   return (
-    <>
-    <div>inputPage</div>
-    
-    </>
+    <div className="flex-col justify-center py-20">
+        <h2 className="text-center text-2xl font-bold">収支登録</h2>
+        <NewBalanceForm />
+    </div>
   )
 }
 
